b7cf37ecb3db5664b82626f5df9a3f97e57955ec
Nick Timmer <ntimmer96@gmail.com>
2021-04-29 20:09:10 -0500

    Guess The Flag first version

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Nick Timmer on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
