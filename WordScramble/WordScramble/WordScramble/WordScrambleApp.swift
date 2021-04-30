36b7a260f9ebe7aa271881b7424df40a4df4bb65
Nick Timmer <ntimmer96@gmail.com>
2021-04-29 20:18:40 -0500

    Word Scramble first version

---
+++ WordScramble/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Nick Timmer on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
