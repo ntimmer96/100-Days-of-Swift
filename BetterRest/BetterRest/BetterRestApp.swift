75ac705f50200371fe03c97b0895af1dc046b9d8
Nick Timmer <ntimmer96@gmail.com>
2021-04-29 20:00:03 -0500

    First version of the Better Rest app

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Nick Timmer on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
