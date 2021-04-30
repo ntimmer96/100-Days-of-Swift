76401c9a449f944bd01ece5b731c0059352546cf
Nick Timmer <ntimmer96@gmail.com>
2021-04-29 20:14:37 -0500

    Moonshot first version

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Nick Timmer on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
