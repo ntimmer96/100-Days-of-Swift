0a260e013c4cec19e47f28b6d85f967a84927c49
Nick Timmer <ntimmer96@gmail.com>
2021-04-29 20:03:43 -0500

    Bookworm first version

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Nick Timmer on 3/30/21.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
