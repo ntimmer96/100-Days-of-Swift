17f75dc19199687fb57afa3fb95e776068fc93b8
Nick Timmer <ntimmer96@gmail.com>
2021-05-25 18:24:46 -0500

    Bucket List v1.0 - removed all other projects from this

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Nick Timmer on 5/20/21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/BucketList/MKPointAnnotation-ObservableObject.swift
@@ -0,0 +1,29 @@
+//
+//  MKPointAnnotation-ObservableObject.swift
+//  BucketList
+//
+//  Created by Nick Timmer on 5/21/21.
+//
+
+import MapKit
+
+extension MKPointAnnotation: ObservableObject {
+    public var wrappedTitle: String {
+        get {
+            self.title ?? "Unknown value"
+        }
+        set {
+            title = newValue
+        }
+    }
+    
+    public var wrappedSubtitle: String {
+        get {
+            self.subtitle ?? "Unknown value"
+        }
+        set {
+            subtitle = newValue
+        }
+    }
+}
+
